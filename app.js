6c5d58c0de5777028d5885341033978d052f764d
Rian Priansyah <rian.priansyah04@gmail.com>
2023-09-21 14:13:36 +0700

    membuat model place dan contoh datanya

--- app.js
+++ app.js
@@ -1,14 +1,39 @@
 // "C:\Program Files\MongoDB\Server\5.0\bin\mongod.exe" --dbpath="c:\data\db"
 
 const express = require("express");
+const mongoose = require("mongoose");
 const path = require("path");
 const app = express();
+
+// models
+const Place = require("./models/place");
+
+// connect to mongodb
+mongoose
+  .connect("mongodb://127.0.0.1/bestpoints")
+  .then((result) => {
+    console.log("connected to mongodb");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
   res.render("home");
+});
+
+app.get("/seed/places", async (req, res) => {
+  const place = new Place({
+    title: "Empire State Building",
+    price: "$99999999",
+    description: "A great building",
+    location: "New York, NY",
+  });
+  await place.save();
+  res.send(place);
 });
 
 app.listen(3000, () => {

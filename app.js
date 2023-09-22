10a9facfdc1419f407055e7ef2a50d6f9dbaf3db
Rian Priansyah <rian.priansyah04@gmail.com>
2023-09-22 15:38:56 +0700

    membuat error handler beserta tampilannya

--- app.js
+++ app.js
@@ -2,13 +2,16 @@
 
 const ejsMate = require("ejs-mate");
 const express = require("express");
+const ExpressError = require("./utils/ExpressError");
 const methodOverride = require("method-override");
 const mongoose = require("mongoose");
+const wrapAsync = require("./utils/wrapAsync");
 const path = require("path");
 const app = express();
 
 // models
 const Place = require("./models/place");
+const { error } = require("console");
 
 // connect to mongodb
 mongoose
@@ -33,47 +36,67 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
-app.get("/places", async (req, res) => {
-  const places = await Place.find();
-  res.render("places/index", { places });
-});
+app.get(
+  "/places",
+  wrapAsync(async (req, res) => {
+    const places = await Place.find();
+    res.render("places/index", { places });
+  })
+);
 
 app.get("/places/create", (req, res) => {
   res.render("places/create");
 });
 
-app.post("/places", async (req, res, next) => {
-  try {
+app.post(
+  "/places",
+  wrapAsync(async (req, res, next) => {
     const place = new Place(req.body.place);
     await place.save();
     res.redirect("/places");
-  } catch (error) {
-    next(error);
-  }
-});
+  })
+);
 
-app.get("/places/:id", async (req, res) => {
-  const place = await Place.findById(req.params.id);
-  res.render("places/show", { place });
-});
+app.get(
+  "/places/:id",
+  wrapAsync(async (req, res) => {
+    const place = await Place.findById(req.params.id);
+    res.render("places/show", { place });
+  })
+);
 
-app.get("/places/:id/edit", async (req, res) => {
-  const place = await Place.findById(req.params.id);
-  res.render("places/edit", { place });
-});
+app.get(
+  "/places/:id/edit",
+  wrapAsync(async (req, res) => {
+    const place = await Place.findById(req.params.id);
+    res.render("places/edit", { place });
+  })
+);
 
-app.put("/places/:id", async (req, res) => {
-  await Place.findByIdAndUpdate(req.params.id, { ...req.body.place });
-  res.redirect("/places");
-});
+app.put(
+  "/places/:id",
+  wrapAsync(async (req, res) => {
+    await Place.findByIdAndUpdate(req.params.id, { ...req.body.place });
+    res.redirect("/places");
+  })
+);
 
-app.delete("/places/:id", async (req, res) => {
-  await Place.findByIdAndDelete(req.params.id);
-  res.redirect("/places");
+app.delete(
+  "/places/:id",
+  wrapAsync(async (req, res) => {
+    await Place.findByIdAndDelete(req.params.id);
+    res.redirect("/places");
+  })
+);
+
+app.all("*", (req, res, next) => {
+  next(new ExpressError("Page not found", 404));
 });
 
 app.use((err, req, res, next) => {
-  res.status(500).send("Something Broke!");
+  const { statusCode = 500 } = err;
+  if (!err.message) err.message = "Oh No, Something Went Wrong!";
+  res.status(statusCode).render("error", { err });
 });
 
 app.listen(3000, () => {

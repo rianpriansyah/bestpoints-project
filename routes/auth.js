c9440835a9ea5c64e2c268eae00f111dc0366f3c
Rian Priansyah <rian.priansyah04@gmail.com>
2023-09-27 13:58:49 +0700

    membuat halaman login beserta fungsinya

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
 const wrapAsync = require("../utils/wrapAsync");
+const passport = require("passport");
 
 router.get("/register", (req, res) => {
   res.render("auth/register");
@@ -15,12 +16,31 @@ router.post(
       const user = new User({ email, username });
       await User.register(user, password);
       req.flash("success_msg", "You are now registered and can log in");
-      res.redirect("/places");
+      res.redirect("/login");
     } catch (error) {
       req.flash("error_msg", error.message);
       res.redirect("/register");
     }
   })
+);
+
+router.get("/login", (req, res) => {
+  res.render("auth/login");
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureRedirect: "/login",
+    failureFlash: {
+      type: "error_msg",
+      msg: "Invalid username or password",
+    },
+  }),
+  (req, res) => {
+    req.flash("success_msg", "You are now logged in");
+    res.redirect("/places");
+  }
 );
 
 module.exports = router;

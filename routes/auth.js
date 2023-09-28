de8730009a4c64b108c55248c630b9ac813e67ef
Rian Priansyah <rian.priansyah04@gmail.com>
2023-09-28 20:46:54 +0700

    membuat controller untuk review dan user auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,35 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
+const AuthController = require("../controllers/auth");
 const wrapAsync = require("../utils/wrapAsync");
 const passport = require("passport");
 
-router.get("/register", (req, res) => {
-  res.render("auth/register");
-});
+router.get("/register", AuthController.registerForm);
 
-router.post(
-  "/register",
-  wrapAsync(async (req, res) => {
-    try {
-      const { email, username, password } = req.body;
-      const user = new User({ email, username });
-      const registerUser = await User.register(user, password);
-      req.login(registerUser, (err) => {
-        if (err) return next(err);
-        req.flash("success_msg", "You are now registered and logged in");
-        res.redirect("/places");
-      });
-    } catch (error) {
-      req.flash("error_msg", error.message);
-      res.redirect("/register");
-    }
-  })
-);
+router.post("/register", wrapAsync(AuthController.register));
 
-router.get("/login", (req, res) => {
-  res.render("auth/login");
-});
+router.get("/login", AuthController.loginForm);
 
 router.post(
   "/login",
@@ -40,20 +20,9 @@ router.post(
       msg: "Invalid username or password",
     },
   }),
-  (req, res) => {
-    req.flash("success_msg", "You are now logged in");
-    res.redirect("/places");
-  }
+  AuthController.login
 );
 
-router.post("/logout", (req, res) => {
-  req.logout(function (err) {
-    if (err) {
-      return next(err);
-    }
-    req.flash("success_msg", "You are now logged out");
-    res.redirect("/login");
-  });
-});
+router.post("/logout", AuthController.logout);
 
 module.exports = router;
--- routes/reviews.js
+++ routes/reviews.js
@@ -3,6 +3,7 @@ const wrapAsync = require("../utils/wrapAsync");
 const ExpressError = require("../utils/ExpressError");
 const Place = require("../models/place");
 const Review = require("../models/review");
+const ReviewController = require("../controllers/reviews");
 const { reviewSchema } = require("../schemas/review");
 const isValidObjectId = require("../middlewares/isValidObjectId");
 const isAuth = require("../middlewares/isAuth");
@@ -19,36 +20,8 @@ const validateReview = (req, res, next) => {
   }
 };
 
-router.post(
-  "/",
-  isAuth,
-  isValidObjectId("/places"),
-  validateReview,
-  wrapAsync(async (req, res) => {
-    const { place_id } = req.params;
-    const review = new Review(req.body.review);
-    review.author = req.user._id;
-    await review.save();
-    const place = await Place.findById(place_id);
-    place.reviews.push(review);
-    await place.save();
-    req.flash("success_msg", "Review Created!");
-    res.redirect(`/places/${place_id}`);
-  })
-);
+router.post("/", isAuth, isValidObjectId("/places"), validateReview, wrapAsync(ReviewController.store));
 
-router.delete(
-  "/:review_id",
-  isAuth,
-  isAuthorReview,
-  isValidObjectId("/places"),
-  wrapAsync(async (req, res) => {
-    const { place_id, review_id } = req.params;
-    await Place.findByIdAndUpdate(place_id, { $pull: { reviews: review_id } });
-    await Review.findByIdAndDelete(review_id);
-    req.flash("success_msg", "Review Deleted!");
-    res.redirect(`/places/${place_id}`);
-  })
-);
+router.delete("/:review_id", isAuth, isAuthorReview, isValidObjectId("/places"), wrapAsync(ReviewController.delete));
 
 module.exports = router;

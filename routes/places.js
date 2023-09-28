56cd7152a9ed4012644c5820db1f99ba1da4a862
Rian Priansyah <rian.priansyah04@gmail.com>
2023-09-28 14:08:38 +0700

    membuat validasi permission dari sebuah data

--- routes/places.js
+++ routes/places.js
@@ -67,9 +67,15 @@ router.put(
   isValidObjectId("/places"),
   validatePlace,
   wrapAsync(async (req, res) => {
-    await Place.findByIdAndUpdate(req.params.id, { ...req.body.place });
+    const { id } = req.params;
+    let place = await Place.findById(id);
+    if (!place.author.equals(req.user._id)) {
+      req.flash("error_msg", "Not Authorized!");
+      return res.redirect("/places");
+    }
+    place = await Place.findByIdAndUpdate(id, { ...req.body.place });
     req.flash("success_msg", "Place Updated!");
-    res.redirect(`/places/${req.params.id}`);
+    res.redirect(`/places/${id}`);
   })
 );
 

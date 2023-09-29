c9b80538a2ba529277fec37b90d695e02cd797a2
Rian Priansyah <rian.priansyah04@gmail.com>
2023-09-29 18:35:18 +0700

    menghapus data place beserta gambarnya

--- controllers/places.js
+++ controllers/places.js
@@ -52,7 +52,17 @@ module.exports.update = async (req, res) => {
 };
 
 module.exports.delete = async (req, res) => {
-  await Place.findByIdAndDelete(req.params.id);
+  const { id } = req.params;
+  const place = await Place.findById(id);
+
+  if (place.images.length > 0) {
+    place.images.forEach((image) => {
+      fs.unlink(image.url, (err) => new ExpressError(err));
+    });
+  }
+
+  await place.deleteOne();
+
   req.flash("success_msg", "Place Deleted!");
   res.redirect("/places");
 };
